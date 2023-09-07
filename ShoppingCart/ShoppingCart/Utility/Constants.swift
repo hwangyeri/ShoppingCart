d226b3ddce5d10aa5156331295f607a787353489
hwangyeri <dpfl420@naver.com>
2023-09-08 03:15:04 +0900

    [Feat] 디자인 및 컬러 설정 관리 파일 Constants 추가

---
+++ ShoppingCart/ShoppingCart/Utility/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  ShoppingCart
+//
+//  Created by 황예리 on 2023/09/07.
+//
+
+import Foundation
+
+enum Constants {
+    
+    enum Desgin {
+        static let cornerRadius: CGFloat = 8
+        static let borderWidth: CGFloat = 1
+    }
+}
+

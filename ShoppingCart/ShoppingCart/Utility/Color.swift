9fe304c8ea19c90fb767dd364e58421350c3e300
hwangyeri <dpfl420@naver.com>
2023-09-08 03:15:44 +0900

    [Style] Dark Mode 대응을 위한 SystemColor 적용

---
+++ ShoppingCart/ShoppingCart/Utility/Color.swift
@@ -0,0 +1,22 @@
+//
+//  Color.swift
+//  ShoppingCart
+//
+//  Created by 황예리 on 2023/09/07.
+//
+
+import UIKit
+
+extension Constants {
+    
+    enum BaseColor {
+        static let background = UIColor.systemBackground
+        static let border = UIColor.systemGray.cgColor
+        static let text = UIColor.label
+//        static let placeholder = UIColor.systemGray
+        static let subText = UIColor.systemGray
+        static let point = UIColor.systemRed
+    }
+    
+}
+

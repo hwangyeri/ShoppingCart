d49b93e3d104bc78d63222813aea76e78eb68e50
hwangyeri <dpfl420@naver.com>
2023-09-09 03:26:55 +0900

    [Feat] LikeViewController 생성 후 TabBar 적용

---
+++ ShoppingCart/ShoppingCart/Scene/Like/LikeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LikeViewController.swift
+//  ShoppingCart
+//
+//  Created by 황예리 on 2023/09/09.
+//
+
+import UIKit
+
+class LikeViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}

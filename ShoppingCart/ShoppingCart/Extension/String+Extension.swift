afc094c3094d5b24ca83a149735296fd9cea4526
hwangyeri <dpfl420@naver.com>
2023-09-11 22:06:35 +0900

    [Feat] Number Formatter, HTML tags Remove 기능 추가

---
+++ ShoppingCart/ShoppingCart/Extension/String+Extension.swift
@@ -0,0 +1,34 @@
+//
+//  String+Extension.swift
+//  ShoppingCart
+//
+//  Created by 황예리 on 2023/09/11.
+//
+
+import Foundation
+
+extension String {
+    
+    // MARK: - Number Formatter
+    
+    func formatNumber() -> String {
+            let numberFormatter = NumberFormatter()
+            numberFormatter.numberStyle = .decimal
+            if let formattedPrice = numberFormatter.string(from: NSNumber(value: Double(self) ?? 0)) {
+                return formattedPrice
+            }
+            return ""
+        }
+    
+    // MARK: - HTML tags Remove
+    
+    func removeHTMLTags() -> String {
+        let regex = try? NSRegularExpression(pattern: "<[^>]+>", options: .caseInsensitive)
+        let range = NSRange(location: 0, length: self.utf16.count)
+        return regex?.stringByReplacingMatches(in: self, options: [], range: range, withTemplate: "") ?? self
+    }
+    
+    
+}
+
+

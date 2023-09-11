0eb8d970ee580caed07a13d77022668d8fcbb714
hwangyeri <dpfl420@naver.com>
2023-09-11 22:09:36 +0900

    [Feat] WebView 을 통한 상품 링크 페이지 보여주기, 좋아요 기능 구현
    - WebView 및 상품 링크 페이지 보여주기 기능 구현
    - 상세 화면 UI 및 좋아요 기능 구현
    - 검색 화면, 좋아요 목록 화면에서 셀 선택시 해당 데이터로 화면 구성
    - NavigationBar UI 및 상품의 타이틀, 좋아요 상태 보여주는 기능 구현

---
+++ ShoppingCart/ShoppingCart/Scene/Detail/DetailView.swift
@@ -0,0 +1,27 @@
+//
+//  DetailView.swift
+//  ShoppingCart
+//
+//  Created by 황예리 on 2023/09/11.
+//
+
+import UIKit
+import SnapKit
+import WebKit
+
+class DetailView: BaseView {
+    
+    var webView = WKWebView()
+    
+    override func configureView() {
+        addSubview(webView)
+    }
+    
+    override func setConstraints() {
+        
+        webView.snp.makeConstraints { make in
+            make.edges.equalTo(self)
+        }
+    }
+    
+}
---
+++ ShoppingCart/ShoppingCart/Scene/Detail/DetailViewController.swift
@@ -0,0 +1,115 @@
+//
+//  DetailViewController.swift
+//  ShoppingCart
+//
+//  Created by 황예리 on 2023/09/11.
+//
+
+import UIKit
+import WebKit
+import RealmSwift
+
+class DetailViewController: BaseViewController {
+    
+    let mainView = DetailView()
+    var webView = WKWebView()
+    let repositoy = ShoppingTableRepository()
+    var likeButton = UIButton()
+    
+    var searchSelectedItem: Item = Item(title: "", image: "", lprice: "", mallName: "", productID: "")
+    var likeSelectedItem: Item = Item(title: "", image: "", lprice: "", mallName: "", productID: "")
+    
+    var isFromLikeView: Bool = false
+    
+    override func loadView() {
+        self.view = mainView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if isFromLikeView {
+            // 라이크 뷰에서 열린 경우
+            let selectedItem = likeSelectedItem
+            configureDetailView(with: selectedItem)
+        } else {
+            // 서치 뷰에서 열린 경우
+            let selectedItem = searchSelectedItem
+            configureDetailView(with: selectedItem)
+        }
+        
+        // 좋아요 버튼 설정
+        likeButton.setImage(UIImage(systemName: "heart"), for: .normal)
+        likeButton.addTarget(self, action: #selector(likeButtonTapped), for: .touchUpInside)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: likeButton)
+    }
+    
+    override func configure() {
+        super.configure()
+        
+        let navigationAppearance = UINavigationBarAppearance()
+        navigationAppearance.backgroundColor = Constants.BaseColor.background
+        navigationController?.navigationBar.scrollEdgeAppearance = navigationAppearance
+        navigationController?.navigationBar.standardAppearance = navigationAppearance
+        navigationController?.navigationBar.tintColor = Constants.BaseColor.text
+    }
+    
+    // 디테일 뷰 설정
+    func configureDetailView(with selectedItem: Item) {
+        
+        DispatchQueue.main.async {
+            self.title = selectedItem.title.removeHTMLTags()
+        }
+        
+        if let myURL = URL(string: "https://msearch.shopping.naver.com/product/\(selectedItem.productID)") {
+            let myRequest = URLRequest(url: myURL)
+            mainView.webView.load(myRequest)
+        } else {
+            //FIXME: URL 생성 실패 시 처리할 코드 추가하기
+            print("URL 생성 실패")
+        }
+        
+        let duplicateItems = repositoy.duplicateFilterItems(forProductID: selectedItem.productID)
+
+        if duplicateItems.isEmpty {
+            DispatchQueue.main.async {
+                self.likeButton.setImage(UIImage(systemName: "heart"), for: .normal)
+            }
+        } else {
+            DispatchQueue.main.async {
+                self.likeButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+            }
+        }
+        
+    }
+    
+    // MARK: - >>>> 좋아요 버튼 클릭!!! <<<<<
+    
+    @objc func likeButtonTapped() {
+        print(#function)
+        
+        let selectedItem = isFromLikeView ? likeSelectedItem : searchSelectedItem
+        let duplicateItems = repositoy.duplicateFilterItems(forProductID: selectedItem.productID)
+        
+        if duplicateItems.isEmpty {
+            let newItem = ShoppingTable(productID: selectedItem.productID, photo: selectedItem.image, mallName: selectedItem.mallName, title: selectedItem.title, price: selectedItem.lprice, likeDate: Date())
+            repositoy.createItem(newItem)
+            print("새로운 아이템 저장: \(newItem)")
+            DispatchQueue.main.async {
+                self.likeButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+            }
+        } else {
+            if let duplicatedItem = duplicateItems.first {
+                repositoy.deleteItem(duplicatedItem)
+                print("중복 아이템 삭제: \(duplicatedItem)")
+                DispatchQueue.main.async {
+                    self.likeButton.setImage(UIImage(systemName: "heart"), for: .normal)
+                }
+            }
+        }
+        
+        NotificationCenter.default.post(name: NSNotification.Name("DataDidChange"), object: nil)
+    }
+    
+    
+}

e2b1fa6c19d59cb741194550aec5a3f063b41944
hwangyeri <dpfl420@naver.com>
2023-09-23 22:45:13 +0900

    [Refactor] Alert+Extension 추가 및 코드 개선
    - showAlertMessage: BaseViewController -> Alert+Extension

--- ShoppingCart/ShoppingCart/Base/BaseViewController.swift
+++ ShoppingCart/ShoppingCart/Base/BaseViewController.swift
@@ -21,15 +21,6 @@ class BaseViewController: UIViewController {
     
     func setConstraints() { }
     
-    func showAlertMessage(message: String, button: String = "확인", handler: (() -> ())? = nil ) {
-        let alert = UIAlertController(title: "잠시만요..!", message: message, preferredStyle: .alert)
-        let ok = UIAlertAction(title: button, style: .default) { _ in
-            handler?()
-        }
-        alert.addAction(ok)
-        present(alert, animated: true)
-    }
-    
 }
 
 
---
+++ ShoppingCart/ShoppingCart/Extension/Alert+Extension.swift
@@ -0,0 +1,23 @@
+//
+//  Alert+Extension.swift
+//  ShoppingCart
+//
+//  Created by 황예리 on 2023/09/23.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    // MARK: - Alert
+    
+    func showAlertMessage(message: String, button: String = "확인", handler: (() -> ())? = nil ) {
+        let alert = UIAlertController(title: "잠시만요..!", message: message, preferredStyle: .alert)
+        let ok = UIAlertAction(title: button, style: .default) { _ in
+            handler?()
+        }
+        alert.addAction(ok)
+        present(alert, animated: true)
+    }
+    
+}
